9601caf8010fc11add891b2c7bde467f0c4071a1
saiakhiltechie-NRI <saiakhiltechie@nriinfratech.com>
2026-05-12 08:41:59 +0530

    Update auth-middleware.ts

--- src/integrations/supabase/auth-middleware.ts
+++ src/integrations/supabase/auth-middleware.ts
@@ -74,6 +74,7 @@ export const requireSupabaseAuth = createMiddleware({ type: 'function' }).server
         supabase,
         userId: data.claims.sub,
         claims: data.claims,
-      }
+      },
+    });
   }
 )
